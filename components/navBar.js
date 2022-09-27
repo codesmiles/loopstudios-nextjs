119c0eb2bdbcd6343b0557b1ef27e23d64411b37
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-09-28 00:31:54 +0100

    update for later today

--- components/navBar.js
+++ components/navBar.js
@@ -1,31 +1,35 @@
 import NavList from "./sub/navList";
 import Hamburger from "./sub/hamburger";
 import Logo from "./sub/logo";
-import { useState } from "react";
 
 const Navbar = () => {
-  const [show, setShow] = useState(false);
-  //   const [windowWidth,setWindowWidth] = useState(window.innerWidth);
-
-  const hamburgerStyle = {
-    display: !show ? "block" : "none",
-  };
-
   return (
     <nav className="w-4/5 my-0 mx-auto flex justify-between pt-14">
-      <Logo/>
-      <div
-        onClick={() => {
-          setShow(true);
-        }}
-        className="lg:hidden"
-      >
-        <Hamburger />
+      <div>
+        <Logo />
       </div>
-      {/* {show && <NavList />} */}
-      <div className="hidden lg:block">
-        <NavList />
+
+      <div>
+        <div className="lg:hidden">
+          <Hamburger />
+        </div>
+
+        <div className="hidden lg:block">
+          <NavList />
+        </div>
       </div>
+
+      {/* <div>
+        <div>
+        //HEAD
+          <Logo />
+          <img src="/images/icon-close.svg" alt="" />
+        </div>
+        <div>
+        BODY
+          <NavList />
+        </div>
+      </div> */}
     </nav>
   );
 };
