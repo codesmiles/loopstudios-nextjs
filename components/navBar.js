28813d58dccc7858023e02202e0aafad88a62b06
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-09-28 18:14:53 +0100

    Complete TFN update

--- components/navBar.js
+++ components/navBar.js
@@ -3,21 +3,24 @@ import Hamburger from "./sub/hamburger";
 import Close from "./sub/close";
 import Logo from "./sub/logo";
 import { useState } from "react";
+import {useRouter} from "next/router"
+import { useEffect } from "react";
 
 const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const {events} = useRouter();
 
-  const handleNavOpen = () => {
-    if (window.innerWidth < 768) {
-      isNavOpen ? (
-        <div
-          className={` absolute pl-14 left-0 top-0 pt-40 bg-black w-full h-full md:bg-transparent md:static md:pl-0 md:pt-2`}
-        >
-          <NavList />
-        </div>
-      ) : null;
+  const close = () =>{
+    isNavOpen ? setIsNavOpen(false) : setIsNavOpen(true);
+  }
+  useEffect(()=>{
+    events.on("routeChangeStart",close);
+
+    return ()=>{
+      events.off("routeChangeStart",close)
     }
-  };
+  },[close,events])
+
   return (
     <nav className="w-4/5 mx-auto flex justify-between py-7">
       <div className="z-20">
@@ -45,9 +48,7 @@ const Navbar = () => {
 
         <span
           className="cursor-pointer md:hidden block "
-          onClick={() => {
-            isNavOpen ? setIsNavOpen(false) : setIsNavOpen(true);
-          }}
+          onClick={close}
         >
           {isNavOpen ? <Close /> : <Hamburger />}
         </span>
